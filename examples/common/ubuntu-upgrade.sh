f100b18f4f3ecf3e9a07e129f0a4aa9ebbb67b93
doig-u <itcorps@gmail.com>
2017-03-15 16:43:19 +0300

    Do not "apt-get update" if it was done less than 1 day ago.

--- examples/common/ubuntu-upgrade.sh
+++ examples/common/ubuntu-upgrade.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ex
 
-apt-get update
+cur=$(date +%s)
+upd=$(stat -c %Y /var/cache/apt/pkgcache.bin)
+if (( $upd + 86400 < $cur )); then
+    apt-get update
+fi
 
 # it is for switch off interaction when grub upgrades (possible only for Vagrant Ubuntu 16.04)
 DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
